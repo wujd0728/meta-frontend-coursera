707f4c28199ed61c5239156d6445629e7731d946
dj5 <davidwujd@gmail.com>
2024-05-20 09:42:02 -0500

    starting JS

---
+++ introduction to JS/first JS code.js
@@ -0,0 +1,11 @@
+// this is a single-line comment!
+
+/*
+this is a
+multi-line
+comment
+*/
+
+/* this is a multi-line comment on a single line */
+
+console.log("%cHello, world", "color: blue; font-size: 40px")
