596ac81d496e04aa3adcf82b29930b8ed5c40b9f
dj5 <davidwujd@gmail.com>
2024-05-23 18:11:06 -0500

    js oop

---
+++ introduction to JS/for of and for in to iterate objects.js
@@ -0,0 +1,35 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+function logDairy(){
+    for (var item of dairy) {
+        console.log(item)
+    }
+}
+logDairy()
+// Task 2
+const animal = {
+
+canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (var prop of Object.keys(bird)) {
+        console.log(prop +": " + bird[prop])
+    }
+}
+birdCan()
+// Task 3
+
+function animalCan() {
+    for (var item in bird) {
+        console.log(item + ": " + bird[item])
+    }
+}
+animalCan()
